2921314aae75635c1ccf63834488296f9acbe7f5
Gaik Artur <141954990+webDevArtur@users.noreply.github.com>
2024-09-22 03:58:15 +0300

    Add files via upload

--- src/pages/HomePage/components/TeamSection/TeamSection.tsx
+++ src/pages/HomePage/components/TeamSection/TeamSection.tsx
@@ -1,11 +1,13 @@
 import styles from './TeamSection.module.scss';
+import member1 from './assets/member1.png';
+import member2 from './assets/member2.png';
 
 const TeamSection = () => {
   const teamMembers = [
-    { name: 'Константинова Елена', image: 'src/assets/member1.png' },
-    { name: 'Дубов Леонид', image: 'src/assets/member2.png' },
-    { name: 'Громушкина Елена', image: 'src/assets/member1.png' },
-    { name: 'Трухин Алексей', image: 'src/assets/member2.png' },
+    { name: 'Константинова Елена', image: member1 },
+    { name: 'Дубов Леонид', image: member2 },
+    { name: 'Громушкина Елена', image: member1 },
+    { name: 'Трухин Алексей', image: member2 },
   ];
 
   return (
